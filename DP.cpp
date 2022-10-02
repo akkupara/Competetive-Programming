68a991559444e18b652727ca2140f6bae48e9717
Deeksha A <109099940+deeksha211@users.noreply.github.com>
2022-10-02 09:43:00 +0530

    Update DP.cpp

--- DP.cpp
+++ DP.cpp
@@ -94,4 +94,271 @@ public:
 
 
 
+3.) Max Element without adjacents
+
+Example 1:
+
+Input:
+N = 6
+Arr[] = {5, 5, 10, 100, 10, 5}
+Output: 110
+Explanation: If you take indices 0, 3
+and 5, then Arr[0]+Arr[3]+Arr[5] =
+5+100+5 = 110.
+Example 2:
+
+Input:
+N = 4
+Arr[] = {3, 2, 7, 10}
+Output: 13
+Explanation: 3 and 10 forms a non
+continuous  subsequence with maximum
+sum.
+    
+    
+    
+
+--> RECURSIVE SOLUTION TAKES MUCH LONGER EXECUTION TIME WHICH CAN BE FURTHER OPTIMISED
+
+
+--> Recursive solution:
+
+class Solution{
+public:	//RECURSIVE SOLUTION WHICH TAKES LONG EXECUTION TIME BECAUSE OF OVERLAPPING SUB-PROBLEMS
+    int f(int ind, int *arr)
+    {
+        if(ind == 0)
+            return arr[ind];//if index is equal to 0, then return arr[ind], becuz we r going to pick it up
+        if(ind < 0)
+            return 0;//if index goes less than 0, then return 0
+            
+        int pick = arr[ind] + f(ind-2, arr);//if you pick it then add that index value with element that is non-adjacent
+        int notpick = 0 + f(ind-1, arr);//if you dont pick it, then dont add that index but add only the adjacent element
+        
+        return max(pick, notpick);
+    }
+	// calculate the maximum sum with out adjacent
+	int findMaxSum(int *arr, int n) {
+	    return f(n-1, arr);//the recursive function which will find the max sum, where we pass index till which the iteration should be done
+	}
+};
+
+
+--> we can use a dp array to space optimise it which can avoid TLE
+
+DP SOLUTION:
+
+
+class Solution{
+public:	//USING DP TO SPACE OPTIMISE TO SOME EXTENT
+    int f(int ind, int *arr, vector<int> &dp)
+    {
+        if(ind == 0)
+            return arr[ind];//if index is equal to 0, then return arr[ind], becuz we r going to pick it up
+        if(ind < 0)
+            return 0;//if index goes less than 0, then return 0
+        
+        
+        if(dp[ind] != -1)
+            return dp[ind];
+        
+        int pick = arr[ind] + f(ind-2, arr, dp);//if you pick it then add that index value with element that is non-adjacent
+        int notpick = 0 + f(ind-1, arr, dp);//if you dont pick it, then dont add that index but add only the adjacent element
+        
+        return dp[ind] = max(pick, notpick);
+    }
+	// calculate the maximum sum with out adjacent
+	int findMaxSum(int *arr, int n) {
+	    vector<int> dp(n, -1);
+	    return f(n-1, arr, dp);//the recursive function which will find the max sum, where we pass index till which the iteration should be done
+	}
+};
+
+
+
+using tabulation the Tc = O(N) and SC = O(N) which can further be space optimised
+
+TABULATION:
+
+class Solution{
+public:	//USING tabulation 
+    
+	int findMaxSum(int *arr, int n) {
+	    vector<int> dp(n+1, -1);
+	    dp[0] = 0;
+	    int neg = 0;//if the index goes negative, we return 0
+	    
+	    
+	    for(int i=1; i < n; i++)
+	    {
+	        int take = arr[i];
+	        if(i > 1)
+	            take += dp[i-2];//this is to ensure that index i-2 doesnt go less than zero
+	        
+	        int nontake = 0 + dp[i-1];
+	        
+	        dp[i] = max(take, nontake);
+	    }
+	    return dp[n-1];//the recursive function which will find the max sum, where we pass index till which the iteration should be done
+	}
+};
+
+
+
+
+
+Tabulation with space optimisation: TC = O(N) & SC = O(1)
+    
+    
+    
+class Solution{
+public:	//USING tabulation with space optimisation
+    
+int findMaxSum(int *arr, int n) {
+    int prev = arr[0];
+    int prev2 = 0;
+    
+    for(int i=1; i < n; i++)
+    {
+        int take = arr[i];
+        if(i > 1)
+            take = take + prev2;
+            
+        int nontake = 0 + prev;
+        
+        int curi = max(take, nontake);
+        
+        prev2 = prev;
+        prev = curi;
+        
+    }
+    return prev;
+}
+};
+
+
+
+
+
+4.) House robber
+
+--> same as the above qs if the houses are in a straight line
+--> Slight change in the logic if the houses are in a circle, since there we should not pick first and last, since they are together(Adjacent)
+    
+if the houses are in a stright line
+
+
+int prev = arr[0];
+    int prev2 = 0;
+    
+    for(int i=1; i < n; i++)
+    {
+        int take = arr[i];
+        if(i > 1)
+            take = take + prev2;
+            
+        int nontake = 0 + prev;
+        
+        int curi = max(take, nontake);
+        
+        prev2 = prev;
+        prev = curi;
+        
+    }
+    return prev;
+
+
+
+if the houses are in a circle
+
+
+class Solution {
+public:
+    long long int max_Non_Adjacent_Sum(vector<int> &nums)
+    {
+        int n = nums.size();
+        int prev = nums[0];
+        int prev2 = 0;
+    
+    for(int i=1; i < n; i++)
+    {
+        int take = nums[i];
+        if(i > 1)
+            take = take + prev2;
+            
+        int nontake = 0 + prev;
+        
+        int curi = max(take, nontake);
+        
+        prev2 = prev;
+        prev = curi;
+        
+    }
+    return prev;
+}
+    
+    
+    int rob(vector<int>& nums) 
+    {
+        int n = nums.size();
+        
+        if(nums.size() == 1)//if there is only one house
+            return nums[0];//return the zero index value
+        
+        vector<int> temp1, temp2;
+        for(int i=0; i < n; i++)
+        {
+            if(i != 0)
+                temp1.push_back(nums[i]);//we push all the values of nums vector into temp1 except for the first index
+            
+            if(i != n-1)
+                temp2.push_back(nums[i]);//we push all the values of nums vector into temp1 except for the last index
+            
+        }
+        
+        int first = max_Non_Adjacent_Sum(temp1);//we are leaving everthing except first element
+        int last = max_Non_Adjacent_Sum(temp2);//we are leaving everthing except last element
+        
+        return max(first, last);//and returning the max of both the elements
+        
+    }
+};
+
+
+
+5.) Unique paths
+
+Example 1:
+
+
+Input: m = 3, n = 7
+Output: 28
+    
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+    
+    
+--> Can be solved using combinations
+
+
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int N = (m-1) + (n-1);//total no of paths availabel
+        int r = m-1;//we are going to place N in r ways
+        
+        double res = 1;
+        
+        for(int i=1; i <= r; i++)
+        {
+            res = res*(N-r+i)/i;
+        }
+        return int(res);
+        
+    }
+};
+
+
+6.) Unique path with obstacles
+
 
