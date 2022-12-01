7b210a712e8da459dbcc03ea9c5795508e14724e
Deeksha A <109099940+deeksha211@users.noreply.github.com>
2022-12-01 18:56:01 +0530

    Update Part - 2 - Dp_on_Grids.cpp

--- DP/Part - 2 - Dp_on_Grids.cpp
+++ DP/Part - 2 - Dp_on_Grids.cpp
@@ -307,3 +307,94 @@ public:
         return dp[0][0];
     }
 };
+
+
+5.) Maximum/ minimum falling path sum
+
+--> Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix.
+--> A falling path starts at any element in the first row and chooses the element in the next row that is either directly below or diagonally left/right. 
+--> Specifically, the next element from position (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
+
+
+memoization solution
+
+class Solution {
+public:
+    int f(int i, int j, vector<vector<int>>& matrix, vector<vector<int>> &dp)
+    {
+        if(j < 0 || j >= matrix[0].size())
+            return 1e9;
+        if(i==0)
+            return matrix[0][j];
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        int s = matrix[i][j] + f(i-1, j, matrix, dp);
+        int ld = matrix[i][j] + f(i-1, j-1, matrix, dp);
+        int rd = matrix[i][j] + f(i-1, j+1, matrix, dp);
+        
+        return dp[i][j] = min(s, min(ld, rd));
+    }
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        int mini = 1e9;
+        for(int j=0; j < m; j++)
+        {
+            mini = min(mini, f(n-1, j, matrix, dp));
+        }
+        return mini;
+        
+    }
+};
+
+
+
+
+Tabulation solution
+
+
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        int mini = 1e9;
+        for(int j=0; j < m; j++)
+        {
+            dp[0][j] = matrix[0][j];
+        }
+        
+        for(int i=1; i < n; i++)
+        {
+            for(int j=0; j < m; j++)
+            {
+                int s = matrix[i][j] + dp[i-1][j];
+                int ld = matrix[i][j];
+                if(j-1 >= 0)
+                    ld = ld + dp[i-1][j-1];
+                else
+                    ld = 1e9;
+                int rd = matrix[i][j];
+                if(j+1 < m)
+                    rd = rd + dp[i-1][j+1];
+                else
+                    rd = 1e9;
+                
+                dp[i][j] = min(s, min(ld, rd));
+            }
+        }
+        
+        for(int j=0; j < m; j++)
+        {
+            mini = min(mini, dp[n-1][j]);
+        }
+        return mini;
+    }
+    
+};
