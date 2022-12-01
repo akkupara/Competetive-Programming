14347eb521bbca63adc4fa560634fc557b877eca
Deeksha A <109099940+deeksha211@users.noreply.github.com>
2022-12-01 14:59:57 +0530

    Update Part - 2 - Dp_on_Grids.cpp

--- DP/Part - 2 - Dp_on_Grids.cpp
+++ DP/Part - 2 - Dp_on_Grids.cpp
@@ -130,3 +130,180 @@ public:
         
     }
 };
+
+
+
+3.) Minimum path sum
+
+--> Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+
+--> You can only move either down or right at any point in time.
+    
+
+Recursion solution:
+
+class Solution {
+public:
+    int f(int i, int j, vector<vector<int>> &grid)
+    {
+        if(i==0 && j==0)
+            return grid[i][j];
+        if(i < 0 || j < 0)
+            return 1e9;
+        
+        int up = grid[i][j] + f(i-1, j, grid);
+        int left = grid[i][j] + f(i, j-1, grid);
+        
+        return min(up, left);
+        
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        return f(n-1, m-1, grid);
+    }
+};
+
+
+
+
+Tabulation:
+
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        
+        for(int i=0; i < n; i++)
+        {
+            for(int j=0; j < m; j++)
+            {
+                if(i==0 && j==0)
+                    dp[i][j] = grid[i][j];
+                else
+                {
+                    int up = grid[i][j];
+                    if(i>0)
+                        up = up + dp[i-1][j];
+                    else
+                        up = up + 1e9;
+                    
+                    int left = grid[i][j];
+                    if(j>0)
+                        left = left + dp[i][j-1];
+                    else
+                        left = 1e9;
+                    
+                    dp[i][j] = min(left, up);
+                
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
+
+
+
+4.) Triangle
+
+
+--> Given a triangle array, return the minimum path sum from top to bottom
+--> Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+Output: 11
+Explanation: The triangle looks like:
+   2
+  3 4
+ 6 5 7
+4 1 8 3
+The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 
+     
+     
+     
+     
+     
+Recursive solution:
+
+class Solution {
+public:
+    int f(int i, int j, vector<vector<int>>& triangle, int n)
+    {
+        if(i == n-1)
+            return triangle[n-1][j];
+        
+        int down = triangle[i][j] + f(i+1, j, triangle, n);
+        int dg = triangle[i][j] + f(i+1, j+1, triangle, n);
+        
+        return min(down, dg);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        return f(0, 0, triangle, n);
+        
+    }
+};
+
+
+
+
+
+Memoization solution:
+
+class Solution {
+public:
+    int f(int i, int j, vector<vector<int>>& triangle, int n, vector<vector<int>> &dp)
+    {
+        if(i == n-1)
+            return triangle[n-1][j];
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        int down = triangle[i][j] + f(i+1, j, triangle, n, dp);
+        int dg = triangle[i][j] + f(i+1, j+1, triangle, n, dp);
+        
+        return dp[i][j] = min(down, dg);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        //int m = triangle[0].size();
+        
+        vector<vector<int>> dp(n, vector<int> (n, -1));
+        
+        return f(0, 0, triangle, n, dp);
+        
+    }
+};
+
+
+
+Tabulation:
+
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        //int m = triangle[0].size();
+        
+        vector<vector<int>> dp(n, vector<int> (n, 0));
+        
+        for(int j=0; j < n; j++)
+            dp[n-1][j] = triangle[n-1][j];
+        
+        for(int i=n-2; i >= 0; i--)
+        {
+            for(int j=i; j >=0; j--)
+            {
+                int down = triangle[i][j] + dp[i+1][j];
+                int dg = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down, dg);
+            }
+        }
+        return dp[0][0];
+    }
+};
