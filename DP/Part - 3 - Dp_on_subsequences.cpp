5346335a9b65750a5fe21e0009ca7686b5e5b4ca
Deeksha A <109099940+deeksha211@users.noreply.github.com>
2022-12-04 00:10:37 +0530

    Update Part - 3 - Dp_on_subsequences.cpp

--- DP/Part - 3 - Dp_on_subsequences.cpp
+++ DP/Part - 3 - Dp_on_subsequences.cpp
@@ -157,7 +157,19 @@ public:
 --> slight modification
 
 
+Example 1:
 
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+Example 2:
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+	
+	
+	
 
 recususive solution: TC = O(2^N) and SC = O(N) ASS
 
