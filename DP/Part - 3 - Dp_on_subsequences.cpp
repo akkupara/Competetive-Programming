31f7902b1100891221933859776761ee0e94d664
Deeksha A <109099940+deeksha211@users.noreply.github.com>
2022-12-06 00:51:59 +0530

    Update Part - 3 - Dp_on_subsequences.cpp

--- DP/Part - 3 - Dp_on_subsequences.cpp
+++ DP/Part - 3 - Dp_on_subsequences.cpp
@@ -738,5 +738,502 @@ public:
 
 
 
+8.) Target sum
+
+--> You are given an integer array nums and an integer target.
+--> You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer
+in nums and then concatenate all the integers.
+
+Input: nums = [1,1,1,1,1], target = 3
+Output: 5
+Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
+-1 + 1 + 1 + 1 + 1 = 3
++1 - 1 + 1 + 1 + 1 = 3
++1 + 1 - 1 + 1 + 1 = 3
++1 + 1 + 1 - 1 + 1 = 3
++1 + 1 + 1 + 1 - 1 = 3
+
+	
+	
+--> this is similar/same to count partitions with given difference
+--> same code where there is change in base case, the difference/target becomes (totsum-target)/2
+
+
+
+class Solution {
+public:
+    int f(int ind, int target, vector<int>& nums, vector<vector<int>> &dp)
+    {
+        
+        if(ind == 0)
+        {
+            if(nums[0] == 0 && target == 0)//later check out why cuz i have some doubt
+                return 2;//there are 2 possibilites, either take/nontake
+            
+            if(target == 0 || target == nums[0])
+                return 1;//see the video once again
+            
+            return 0;
+        }
+        
+        
+        if(dp[ind][target] != -1)
+            return dp[ind][target];
+        
+        int nontake = f(ind-1, target, nums, dp);
+        int take = 0;
+        
+        if(nums[ind] <= target)
+            take = f(ind-1, target-nums[ind], nums, dp);
+        
+        return dp[ind][target] = take + nontake;
+    }
+    
+    int findWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int> (target+1, -1));
+        return f(n-1, target, nums, dp);
+    }
+        
+    int findTargetSumWays(vector<int>& nums, int target)
+    {
+        int n = nums.size();
+        int totsum = 0;
+        for(int i=0; i < n; i++)
+            totsum += nums[i];
+        
+        if(totsum-target < 0 || (totsum - target) % 2)
+            return false;
+        
+        return findWays(nums, (totsum-target)/2);
+        
+    }
+        
+};
+
+
+
+
+
+9.) Coin change II
+
+--> Subtle changes: In the first qs, we are supposed to return the fewest no of coins that we need to need to make up the amount, 
+we calculate min(take, nontake) and add 1 to take case and initially keep take as INT_MAX.
+	
+--> In this qs, we return the no of combinations that make up the amount, here we calculate take+nontake, and we dont add 1 to the take case
+	and keep take as 0 initially.
+	
+--> And in 1st qs, it is finite supply, whereas, in this qs, it is infinite supply
+
+	
+	
+Recursive solution:
+
+
+class Solution {
+public:
+    int f(int ind, vector<int>& coins, int amount)
+    {
+        if(ind == 0)
+            return (amount % coins[0] == 0);
+        
+        int nontake = 0 + f(ind-1, coins, amount);
+        int take = 0;
+        if(coins[ind] <= amount)
+            take = f(ind, coins, amount-coins[ind]);//you stay at that index itself
+        
+        return (take + nontake);
+    }
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        return f(n-1, coins, amount);
+        
+    }
+};
+
+
+
+
+
+
+Memoized solution:
+
+
+
+
+
+class Solution {
+public:
+    int f(int ind, vector<int>& coins, int amount, vector<vector<int>> &dp)
+    {
+        if(ind == 0)
+            return (amount % coins[0] == 0);
+        
+        if(dp[ind][amount] != -1)
+            return dp[ind][amount];
+        
+        
+        int nontake = 0 + f(ind-1, coins, amount, dp);
+        int take = 0;
+        if(coins[ind] <= amount)
+            take = f(ind, coins, amount-coins[ind], dp);//you stay at that index itself
+        
+        return dp[ind][amount] = (take + nontake);
+    }
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int> (amount+1, -1));
+        return f(n-1, coins, amount, dp);
+        
+    }
+};
+
+
+
+
+
+Tabulated code:
+
+
+
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int> (amount+1, 0));
+        
+        for(int t=0; t <= amount; t++)
+        {
+            dp[0][t] = (t % coins[0] == 0);//base case
+        }
+        
+        for(int ind=1; ind < n; ind++)
+        {
+            for(int t = 0; t <= amount; t++)
+            {
+                int nontake = 0 + dp[ind-1][t];
+                int take = 0;
+                if(coins[ind] <= t)
+                    take = dp[ind][t-coins[ind]];
+                
+                dp[ind][t] = take + nontake;
+            }
+        }
+        return dp[n-1][amount];
+        
+    }
+};
+
+
+
+space optimised:
+
+
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int> (amount+1, 0));
+        
+        vector<int> prev(amount+1, 0), cur(amount+1, 0);
+        
+        for(int t=0; t <= amount; t++)
+        {
+            prev[t] = (t % coins[0] == 0);//base case
+        }
+        
+        for(int ind=1; ind < n; ind++)
+        {
+            for(int t = 0; t <= amount; t++)
+            {
+                int nontake = 0 + prev[t];
+                int take = 0;
+                if(coins[ind] <= t)
+                    take = cur[t-coins[ind]];
+                
+                cur[t] = take + nontake;
+            }
+            prev = cur;
+        }
+        return prev[amount];
+        
+    }
+};
+
+
+
+
+
+
+
+10.) Unbounded knapsack
+
+--> same as 0/1 knapsack where duplicates or infinite supply is allowed
+
+Recursive code:
+
+
+class Solution{
+public:
+    int f(int ind, int W, int val[], int wt[])
+    {
+        if(ind == 0)
+            return ((int)(W / wt[0]))*val[0];
+        
+        int nontake = 0 + f(ind-1, W, val, wt);
+        int take = 0;
+        if(wt[ind] <= W)
+            take = val[ind] + f(ind, W-wt[ind], val, wt);
+        return max(take, nontake);
+    }
+    int knapSack(int N, int W, int val[], int wt[])
+    {
+        return f(N-1, W, val, wt);
+    }
+};
+
+
+
+
+Memoized code:
+
+
+class Solution{
+public:
+    int f(int ind, int W, int val[], int wt[], vector<vector<int>> &dp)
+    {
+        if(ind == 0)
+            return ((int)(W / wt[0]))*val[0];
+        
+        if(dp[ind][W] != -1)
+        return dp[ind][W];
+        
+        
+        int nontake = 0 + f(ind-1, W, val, wt, dp);
+        int take = 0;
+        if(wt[ind] <= W)
+            take = val[ind] + f(ind, W-wt[ind], val, wt, dp);
+            
+            
+        return dp[ind][W] = max(take, nontake);
+    }
+    int knapSack(int N, int W, int val[], int wt[])
+    {
+        vector<vector<int>> dp(N, vector<int> (W+1, -1));
+        return f(N-1, W, val, wt, dp);
+    }
+};
+
+
+
+
+Tabulated code:
+
+
+class Solution{
+public:
+    
+    int knapSack(int N, int W, int val[], int wt[])
+    {
+        vector<vector<int>> dp(N, vector<int> (W+1, 0));
+        for(int w = 0; w <= W; w++)
+        {
+            dp[0][w] = (int(w/wt[0]))*val[0];
+        }
+        
+        for(int ind = 1; ind < N; ind++)
+        {
+            for(int w = 0; w <= W; w++)
+            {
+                int nontake = 0 + dp[ind-1][w];
+                int take = 0;
+                if(wt[ind] <= w)
+                    take = val[ind] + dp[ind][w-wt[ind]];
+                
+                dp[ind][w] = max(take, nontake);
+            }
+        }
+        
+        return dp[N-1][W];
+    }
+};
+
+
+
+
+
+Space optimised code:
+
+
+class Solution{
+public:
+    
+    int knapSack(int N, int W, int val[], int wt[])
+    {
+        
+        vector<int> prev(W+1, 0), cur(W+1, 0);
+        
+        for(int w = 0; w <= W; w++)
+        {
+            prev[w] = (int(w/wt[0]))*val[0];
+        }
+        
+        for(int ind = 1; ind < N; ind++)
+        {
+            for(int w = 0; w <= W; w++)
+            {
+                int nontake = 0 + prev[w];
+                int take = 0;
+                if(wt[ind] <= w)
+                    take = val[ind] + cur[w-wt[ind]];
+                
+                cur[w] = max(take, nontake);
+            }
+            prev = cur;
+        }
+        
+        return prev[W];
+    }
+};
+
+
+
+
+
+
+11. Rod Cutting
+
+
+--> Given a rod of length N inches and an array of prices, price[]. pricei denotes the value of a piece of length i. 
+--> Determine the maximum value obtainable by cutting up the rod and selling the pieces.
+--> similar to unbounded knapsack if approached differently, other way of looking at this qs is collect rod length to make N and maximise the amount
+
+
+Recursive code:
+
+class Solution{
+  public:
+    int f(int ind, int N, int price[])
+    {
+        if(ind == 0)
+            return N*price[0];//see the video once again
+        
+        int nontake = 0 + f(ind-1, N, price);
+        int take = INT_MIN;
+        
+        int rod_length = ind+1;
+        if(rod_length <= N)
+            take = price[ind] + f(ind, N-rod_length, price);
+            
+        return max(take, nontake);
+    }
+    int cutRod(int price[], int n) {
+        return f(n-1, n, price);
+    }
+};
+
+
+
+
+Memoized code:
+
+
+class Solution{
+  public:
+    int f(int ind, int N, int price[], vector<vector<int>> &dp)
+    {
+        if(ind == 0)
+            return N*price[0];//see the video once again
+        
+        
+        if(dp[ind][N] != -1)
+            return dp[ind][N];
+            
+            
+        int nontake = 0 + f(ind-1, N, price, dp);
+        int take = INT_MIN;
+        
+        int rod_length = ind+1;
+        if(rod_length <= N)
+            take = price[ind] + f(ind, N-rod_length, price, dp);
+            
+        return dp[ind][N] = max(take, nontake);
+    }
+    int cutRod(int price[], int n) {
+        vector<vector<int>> dp(n, vector<int> (n+1, -1));
+        return f(n-1, n, price, dp);
+    }
+};
+
+
+
+
+Tabulation code:
+
+
+class Solution{
+  public:
+    int cutRod(int price[], int N) {
+        vector<vector<int>> dp(N, vector<int> (N+1, 0));
+        
+        for(int n = 0; n <= N; n++)
+            dp[0][n] = n * price[0];//base cases
+            
+            
+        for(int ind=1; ind < N; ind++)
+        {
+            for(int n = 0; n <= N; n++)
+            {
+                int nontake = 0 + dp[ind-1][n];
+                int take = INT_MIN;
+                
+                int rod_length = ind+1;
+                if(rod_length <= n)
+                    take = price[ind] + dp[ind][n-rod_length];
+                    
+                dp[ind][n] = max(take, nontake);
+            }
+        }
+        return dp[N-1][N];
+    }
+};
+
+
+
+
+Space optimised code:
+
+
+class Solution{
+  public:
+    int cutRod(int price[], int N) {
+       
+        vector<int> prev(N+1, 0), cur(N+1, 0);
+        
+        for(int n = 0; n <= N; n++)
+            prev[n] = n * price[0];//base cases
+            
+            
+        for(int ind=1; ind < N; ind++)
+        {
+            for(int n = 0; n <= N; n++)
+            {
+                int nontake = 0 + prev[n];
+                int take = INT_MIN;
+                
+                int rod_length = ind+1;
+                if(rod_length <= n)
+                    take = price[ind] + cur[n-rod_length];
+                    
+                cur[n] = max(take, nontake);
+            }
+            prev = cur;
+        }
+        return prev[N];
+    }
+};
+
 
 
