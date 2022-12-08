ceca0b88ede1056e6afeb4436f7269e6053778f7
Deeksha A <109099940+deeksha211@users.noreply.github.com>
2022-12-09 00:24:16 +0530

    Update DP_on_LIS.cpp

--- DP_on_LIS.cpp
+++ DP_on_LIS.cpp
@@ -18,28 +18,141 @@ Output: 1
 
 
 
+Recursive solution:
+
+
 class Solution {
 public:
-    int f(int ind, int prev_ind, vector<int>& nums, int n, vector<vector<int>> &dp)
+    int f(int ind, int prev, vector<int>& nums, int n)
     {
-        if(ind == n)//if it goes out of bound
+        if(ind == n)//base case
             return 0;
         
-        if(dp[ind][prev_ind+1] != -1)
-            return dp[ind][prev_ind+1];
-        int len = 0 + f(ind+1, prev_ind, nums, n, dp);//not pick case
-        //since we dont pick, we move to the next index and unchanging the position of prev_ind since we are not picking it up
-        if(prev_ind == -1 || nums[ind] > nums[prev_ind])//prev = -1, implies that it doesnt have a previous element
-        {
-            len = max(len, 1 + f(ind+1, ind, nums, n, dp));//since we pick it, we move both ind and prev-ind by 1 towards right, where the prev_ind becomes ind and ind becomes ind+1
-            
-        }
-            
-        return dp[ind][prev_ind+1] = len;
+        //not take
+        int len = 0 + f(ind+1, prev, nums, n);//when u nottake, only move ind, keeping prev at the same position
+        
+        if(prev == -1 || nums[prev] <= nums[ind])
+            len = max(len, 1 + f(ind+1, ind, nums, n));//when u take, move both the index
+       
+        return len;
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));//to represent ind = -1, we cannot use arrays, since they start from 0 index, so we use n+1 for prev_ind
-        return f(0, -1, nums, n, dp);
+        
+        return f(0, -1, nums, n);
+        
     }
 };
+
+
+
+
+Memoization:
+
+--> since there is -1 in prev_index, we do a coordinate shift of 1 
+--> declare a dp[N][N+1]
+                (0->n-1)(-1 -> n-1)
+                           changed to (0->n)
+    
+    
+    class Solution {
+public:
+    int f(int ind, int prev, vector<int>& nums, int n, vector<vector<int>> &dp)
+    {
+        if(ind == n)//base case
+            return 0;
+        
+        if(dp[ind][prev+1] != -1)//1 coordinate shift
+            return dp[ind][prev+1];
+        //not take
+        int len = 0 + f(ind+1, prev, nums, n, dp);//when u nottake, only move ind, keeping prev at the same position
+        
+        if(prev == -1 || nums[prev] < nums[ind])
+            len = max(len, 1 + f(ind+1, ind, nums, n, dp));//when u take, move both the index
+       
+        return dp[ind][prev+1] = len;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        
+        
+        return f(0, -1, nums, n, dp);
+        
+    }
+};
+
+
+
+
+Tabulation code:
+
+
+class Solution {
+public:
+    int f(int ind, int prev, vector<int>& nums, int n, vector<vector<int>> &dp)
+    {
+        if(ind == n)//base case
+            return 0;
+        
+        if(dp[ind][prev+1] != -1)//1 coordinate shift
+            return dp[ind][prev+1];
+        //not take
+        int len = 0 + f(ind+1, prev, nums, n, dp);//when u nottake, only move ind, keeping prev at the same position
+        
+        if(prev == -1 || nums[prev] < nums[ind])
+            len = max(len, 1 + f(ind+1, ind, nums, n, dp));//when u take, move both the index
+       
+        return dp[ind][prev+1] = len;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        
+        for(int ind=n-1; ind >= 0; ind--)
+        {
+            for(int prev=ind-1; prev >= -1; prev--)
+            {
+                int len = 0 + dp[ind+1][prev+1];//when u nottake, only move ind, keeping prev at the same position
+        
+                if(prev == -1 || nums[prev] < nums[ind])
+                    len = max(len, 1 + dp[ind+1][ind+1]);//when u take, move both the index
+
+                 dp[ind][prev+1] = len;
+            }
+        }
+        
+        
+        return dp[0][-1+1];
+        
+    }
+};
+
+
+
+Using Binary search:
+
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> temp;
+        temp.push_back(nums[0]);//push the first guy
+        
+        for(int i=1; i < n; i++)
+        {
+            if(nums[i] > temp.back())
+                temp.push_back(nums[i]);
+            else
+            {
+                int ind = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+                temp[ind] = nums[i];
+            }
+        }
+        return temp.size();
+    }
+};
+
+
