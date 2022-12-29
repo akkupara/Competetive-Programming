a84332e473a481503b7e03ded70aef4c59ac3444
Deeksha A <109099940+deeksha211@users.noreply.github.com>
2022-12-29 16:48:33 +0530

    Update arrays.cpp

--- GS/arrays.cpp
+++ GS/arrays.cpp
@@ -177,3 +177,75 @@ public:
         
     }
 };
+
+
+
+
+3.) Next greater element
+
+
+--> arr[] = [1 3 2 4]
+Output:
+3 4 4 -1
+    
+--> Next greater element of an element in the array is the nearest element on the right which is greater than the current element.
+    
+--> If there does not exist next greater of current element, then next greater element for current element is -1. 
+
+
+Brute solution:
+
+
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> nums, int n){
+        vector<long long> ans;
+        int next;
+        for(int i=0; i < n; i++)
+        {
+            next = -1;
+            for(int j=i+1; j < n; j++)
+            {
+                if(nums[i] < nums[j])
+                {
+                    next = nums[j];
+                    break;
+                }
+            }
+            ans.push_back(next);
+        }
+        return ans;
+    }
+};
+
+
+
+
+Optimised Solution:
+
+
+lass Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        vector<long long> ans(n);
+        stack<long long> st;
+        st.push(-1);
+        for(int i = n-1 ; i >= 0 ;i--) {
+            while(st.top() != -1 && arr[i] >= st.top()) 
+                st.pop();
+            
+            ans[i] = st.top();
+            st.push(arr[i]);
+       
+        }
+        return ans;
+    }
+};
+
+
+
+
