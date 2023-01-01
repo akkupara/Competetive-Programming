2093a04954cd995f51b398611329ded2b746f225
Deeksha A <109099940+deeksha211@users.noreply.github.com>
2023-01-01 16:37:49 +0530

    Update Greedy.cpp

--- GS/Greedy.cpp
+++ GS/Greedy.cpp
@@ -36,3 +36,52 @@ public:
         return ans;
     }
 };
+
+
+
+
+2.) Minimise the difference between maximum height - I
+
+--> need to revise it again
+--> Given an array arr[] denoting heights of N towers and a positive integer K, 
+you have to modify the height of each tower either by increasing or decreasing them by K only once.
+  
+--> Find out what could be the possible minimum difference of the height of shortest and longest towers after you have modified each tower.
+
+  
+  
+K = 2, N = 4
+Arr[] = {1, 5, 8, 10}
+Output:
+5
+Explanation:
+The array can be modified as 
+{3, 3, 6, 8}. The difference between 
+the largest and the smallest is 8-3 = 5.
+  
+  
+  
+  
+  
+class Solution{   
+public:
+    int getMinDiff(int arr[], int n, int k) {
+        // code here
+        sort(arr, arr+n);
+        int currmin = arr[0];
+        int currmax = arr[n-1];
+        int ans = currmax - currmin;
+        
+        for(int i=1; i < n; i++)
+        {
+            currmin = min(arr[0]+k, arr[i]-k);
+            currmax = max(arr[i-1]+k, arr[n-1]-k);
+            ans = min(ans, currmax-currmin);
+        }
+        return ans;
+    }
+};
+
+
+
+
