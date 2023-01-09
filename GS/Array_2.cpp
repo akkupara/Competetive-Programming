c95ab439d850012e19ec432a2bdb7908892e241b
Deeksha A <109099940+deeksha211@users.noreply.github.com>
2023-01-09 15:17:26 +0530

    Update Array_2.cpp

--- GS/Array_2.cpp
+++ GS/Array_2.cpp
@@ -64,6 +64,83 @@ public:
 
 
 
+QS) Majority element II
+
+Find the majority element that appears more than n/3 times (there can be more than two majority elements)
+  
+Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: [3]
+
+Example 2:
+
+Input: nums = [1]
+Output: [1]
+
+Example 3:
+
+Input: nums = [1,2]
+Output: [1,2]
+
+
+
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int num1 = -1, num2 = -1;
+        int c1 = 0, c2 = 0;
+        for(int i=0; i < n; i++)
+        {
+            if(nums[i] == num1)
+                c1++;
+            else if(nums[i] == num2)
+                c2++;
+            else if(c1 == 0)
+            {
+                num1 = nums[i];
+                c1 = 1;
+            }
+            else if(c2 == 0)
+            {
+                num2 = nums[i];
+                c2 = 1;
+            }
+            else
+            {
+                c1--;
+                c2--;
+            }
+        }
+        
+        vector<int> ans;
+        c1 = c2 = 0;
+        
+        for(int i=0; i < n; i++)
+        {
+            if(nums[i] == num1)
+                c1++;
+            else if(nums[i] == num2)
+                c2++;
+        }
+        if(c1 > n/3)
+            ans.push_back(num1);
+        if(c2 > n/3)
+            ans.push_back(num2);
+        
+        return ans;
+    }
+};
+
+
+
+
 2.) Largest consecutive subsequence
 
 brute: first sort and check the largest consecutive subsequence TC = O(n log n) + O(N) and SC = O(1)
@@ -123,3 +200,69 @@ public:
         return largestStreak;
     }
 };
+
+
+
+4.) Largest Subarray with 0 sum
+
+--> Naive solution: check all subarrays which has zero sum and has the greatest length
+TC = O(N*N)
+  
+  
+class Solution{
+       int maxi = 0;
+      public:
+    int maxLen(vector<int>&nums, int n)
+    {   
+       for(int i=0; i < n; i++)
+        {
+            int sum = 0;
+            for(int j=i; j < n; j++)
+            {
+                sum = sum + nums[j];
+                
+                if(sum == 0)
+                    maxi = max(maxi, j-i+1);
+            }
+        }
+        return maxi;
+    }
+};
+
+
+  
+--> Optimal solution:
+
+--------- we are going to maintain a hashmap of (prefix-sum, index) and check if that prefix-sum occurs again in the hashmap and 
+  then subtract the indexes to update the maximum length subarray
+  
+--> tc = 0(n) AND SC = O(N) for the hashmap
+
+
+class Solution{
+    public:
+    int maxLen(vector<int>&nums, int n)
+    {   
+        unordered_map<int, int> mpp;
+        int maxi = 0;
+        int sum = 0;
+        
+        for(int i=0; i < n; i++)
+        {
+            sum = sum + nums[i];
+            if(sum == 0)
+                maxi = i + 1;//from the first index till the current index the subarray adds to 0
+            else
+            {
+                if(mpp.find(sum) != mpp.end())//if we encounter that sum again
+                {
+                    maxi = max(maxi, i-mpp[sum]);
+                }
+                else
+                    mpp[sum] = i;//if the prefuix sum doesnt appear, then just insert it into the hashmap
+            }
+        }
+        return maxi;
+    }
+};
+
