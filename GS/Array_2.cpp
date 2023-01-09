9ef1b208ba6a93d5421d4ef6b3093f71c0dadb8c
Deeksha A <109099940+deeksha211@users.noreply.github.com>
2023-01-09 14:30:55 +0530

    Update Array_2.cpp

--- GS/Array_2.cpp
+++ GS/Array_2.cpp
@@ -64,5 +64,62 @@ public:
 
 
 
+2.) Largest consecutive subsequence
+
+brute: first sort and check the largest consecutive subsequence TC = O(n log n) + O(N) and SC = O(1)
   
+  int longestConsecutive(vector<int> nums) {
+        if(nums.size() == 0 ){
+            return 0;
+        }
+        
+        sort(nums.begin(),nums.end());
+        
+        int ans = 1;
+        int prev = nums[0];
+        int cur = 1;
+        
+        for(int i = 1;i < nums.size();i++){
+            if(nums[i] == prev+1){
+                cur++;
+            }
+            else if(nums[i] != prev){
+                cur = 1;
+            }
+            prev = nums[i];
+            ans = max(ans, cur);
+        }
+        return ans;
+    }
   
+Optimal approah: using hashmap(set in c++) TC = O(N) and Sc = O(N)
+  
+
+  class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        set<int> hashset;//create a hashmap 
+        for(int num: nums)
+            hashset.insert(num);//insert all the elements into the hashset
+        
+        int largestStreak = 0;
+        
+        for(int num: nums)
+        {
+            //check if nums[i]-1 exists in the hashmap
+            if(!hashset.count(num-1))//if it does not exist then check if nums[i]+1 exists or not
+            {
+                int currnum = num;
+                int currentStreak = 1;
+                
+                while(hashset.count(currnum+1))//if curnum+1 existes in the hashmap, then proceed
+                {
+                    currnum += 1; //increment it by 1 sicne all of them are in ascending order
+                    currentStreak += 1;
+                }
+                largestStreak = max(largestStreak, currentStreak);
+            }
+        }
+        return largestStreak;
+    }
+};
